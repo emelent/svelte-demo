3add5e6e5e415a20a595e88713d2d0d77f7bfda9
emelent <roy.motloutsi@gmail.com>
2023-07-27 09:52:45 +0200

    Slide mileage

--- src/lib/CarSearch.svelte
+++ src/lib/CarSearch.svelte
@@ -8,17 +8,33 @@
             .join("")
     const minYear = 2010
 
+    const mileage_limit = 500000
     let min_mileage = 0
     let max_mileage = 50000
+    let mileage_step = 5000
 
     let carsPromise = null
     let years = [...Array(new Date().getFullYear() - minYear) + 1].map(
         (x, i) => minYear + i
     )
+    let provinces = [
+        'Gauteng',
+        'Limpopo',
+        'Northern Cape',
+        'Eastern Cape',
+        'Western Cape',
+        'North West Province',
+        'Mpumalanga',
+        'Kwazulu Natal',
+        'Free State'
+    ]
+
     let selectedYear
+    let selectedProvince
+
     function handleSearch() {
         carsPromise = fetch(
-            `https://api.cars.co.za/fw/public/v3/vehicle?page[offset]=0&page[limit]=120&make_model_variant${model_variant}[All]&sort[date]=desc&mileage[0]=${min_mileage}-${max_mileage}&year[0]=${selectedYear}-${selectedYear}`,
+            `https://api.cars.co.za/fw/public/v3/vehicle?page[offset]=0&page[limit]=120&make_model_variant${model_variant}[All]&sort[date]=desc&mileage[0]=${min_mileage}-${max_mileage}&year[0]=${selectedYear}-${selectedYear}${selectedProvince? "&province[0]=" + encodeURIComponent(selectedProvince):""}`,
             {
                 body: null,
                 method: "GET",
@@ -52,59 +68,88 @@
         style: "currency",
         currency: "ZAR",
     })
+
     function formatCurrency(value) {
         return currencyFormat.format(value)
     }
+
+    const kmFormat = new Intl.NumberFormat("en-ZA", {
+    style: "unit",
+    unit: "kilometer",
+    })
+    function formatKm(value) {
+        return kmFormat.format(value)
+    }
+
 </script>
 
-<h2>Mileage (km)</h2>
-<div class="w-[400px]">
+<h2 class="font-bold">Mileage</h2>
+<div class="">
     <label class="label">
-        <span class="label-text">Min</span>
-        <span class="label-text-alt">Max</span>
+        <span class="label-text">Minimum</span>
+        <span class="label-text-alt font-bold">{formatKm(min_mileage)}</span>
+        
     </label>
-    <div class="join">
-        <input
-            type="number"
-            bind:value={min_mileage}
-            class="input input-bordered join-item"
-        />
-        <input
-            type="number"
-            bind:value={max_mileage}
-            class="input input-bordered join-item"
-        />
-    </div>
+    <input
+        type="range"
+        min={0}
+        max={mileage_limit - mileage_step}
+        step={mileage_step}
+        bind:value={min_mileage}
+        class="range"
+    />
 </div>
 <div>
     <label class="label">
-        <span class="label-text">Search Car {model_variant}</span>
+        <span class="label-text">Maximum</span>
+        <span class="label-text-alt font-bold">{formatKm(max_mileage)}</span>
     </label>
+    <input
+        type="range"
+        min={min_mileage}
+        max={mileage_limit}
+        step={mileage_step}
+        bind:value={max_mileage}
+        class="range"
+    />
+</div>
+<br/>
+<div>
+    <h2 class="mb-3 font-bold">Car</h2>
+    
     <div class="join">
         <input
             bind:value={make_model}
             class="input input-bordered join-item"
-            placeholder="e.g. Ford Figo"
+            placeholder="e.g. Ford;Figo"
         />
 
         <select
             class="select select-bordered join-item"
             bind:value={selectedYear}
         >
-            <option disabled selected>Year</option>
+            <option selected>Year</option>
             {#each years as year}
                 <option value={year}>
                     {year}
                 </option>
             {/each}
         </select>
-        <div class="indicator">
-            <button on:click={handleSearch} class="btn join-item btn-primary"
-                >Search</button
-            >
-        </div>
     </div>
 </div>
+<select
+    class="select select-bordered my-8"
+    bind:value={selectedProvince}
+>
+    <option selected value="">Any province</option>
+    {#each provinces as province}
+        <option value={province}>
+            {province}
+        </option>
+    {/each}
+</select>
+
+<button on:click={handleSearch} class="btn join-item btn-primary">Search</button>
 
 <br/>
 <br/>
