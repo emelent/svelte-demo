e5d668adfa0bf40fd5ad133984b32efd5086ffbe
Roy <roy.motloutsi@gmail.com>
2023-07-29 22:40:29 +0200

    comment

--- src/lib/ui/pages/CarAverage.svelte
+++ src/lib/ui/pages/CarAverage.svelte
@@ -2,7 +2,6 @@
     import { fetchCars } from "../../repository/cars"
     import type { CarData } from "../../repository/types"
     import { formatCurrency } from "../../utils/formatting"
-    import { capitalizeEachWord } from "../../utils/strings"
     import { getCurrentYear } from "../../utils/time"
     import CarMileage from "../components/CarMileage.svelte"
     import CarModel from "../components/CarModel.svelte"
@@ -28,55 +27,44 @@
     let selectedProvince
     let model_search
 
-
     function handleSearch() {
-
         carsPromise = fetchCars({
             maxMileage: max_mileage,
             minMileage: min_mileage,
             province: selectedProvince,
             year: model_year,
-            search: model_search
+            search: model_search,
         })
     }
 
     function getAvgPrice(cars: Array<CarData>): number {
-        return cars.reduce(
-                (total, car) => total + car.price, 
-                0
-            ) / cars.length
+        return cars.reduce((total, car) => total + car.price, 0) / cars.length
     }
 
     function getMinCar(cars: Array<CarData>): CarData {
         return cars.reduce(
-            (prevCar, car) =>
-                car.price < prevCar.price ? car : prevCar,
+            (prevCar, car) => (car.price < prevCar.price ? car : prevCar),
             cars[0]
         )
     }
     function getMaxCar(cars: Array<CarData>): CarData {
         return cars.reduce(
-            (prevCar, car) =>
-                car.price > prevCar.price ? car : prevCar,
+            (prevCar, car) => (car.price > prevCar.price ? car : prevCar),
             cars[0]
         )
     }
-
 </script>
 
-    <h2 class="font-bold">Mileage</h2>
-    <CarMileage 
-        bind:max_mileage={max_mileage}
-        bind:min_mileage={min_mileage}
-    />
+<h2 class="font-bold">Mileage</h2>
+<CarMileage bind:max_mileage bind:min_mileage />
 
-    <h2 class="mb-3 font-bold">Car</h2>
-    <CarModel 
-        bind:search={model_search}
-        bind:year={model_year}
-    />
+<h2 class="mb-3 font-bold">Car</h2>
+<CarModel bind:search={model_search} bind:year={model_year} />
 
-<select class="select select-bordered my-8 block w-full" bind:value={selectedProvince}>
+<select
+    class="select select-bordered my-8 block w-full"
+    bind:value={selectedProvince}
+>
     <option selected value="">Any province</option>
     {#each provinces as province}
         <option value={province}>
@@ -85,7 +73,8 @@
     {/each}
 </select>
 
-<button on:click={handleSearch} class="btn block m-auto join-item btn-primary">Search</button
+<button on:click={handleSearch} class="btn block m-auto join-item btn-primary"
+    >Search</button
 >
 
 {#if carsPromise}
@@ -97,6 +86,7 @@
             {@const maxCar = getMaxCar(cars)}
             {@const minCar = getMinCar(cars)}
             <div class="stats stats-vertical lg:stats-horizontal shadow">
+                <!-- average -->
                 <div class="stat">
                     <div class="stat-title">Average</div>
                     <div class="stat-value">
@@ -104,11 +94,8 @@
                     </div>
                 </div>
 
-                <a
-                    href={maxCar.infoUrl}
-                    target="_blank"
-                    class="stat"
-                >
+                <!-- highest price -->
+                <a href={maxCar.infoUrl} target="_blank" class="stat">
                     <div class="stat-title">Highest</div>
                     <div class="stat-value text-2xl text-neutral-content">
                         {formatCurrency(maxCar.price)}
@@ -122,11 +109,8 @@
                     </div>
                 </a>
 
-                <a
-                    href={minCar.infoUrl}
-                    target="_blank"
-                    class="stat"
-                >
+                <!-- lowest price -->
+                <a href={minCar.infoUrl} target="_blank" class="stat">
                     <div class="stat-title">Lowest</div>
                     <div class="stat-value text-2xl text-neutral-content">
                         {formatCurrency(minCar.price)}
